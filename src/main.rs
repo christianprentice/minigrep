ea80a4b5de448c7f20c24c1eb5f3388998b01ac0
Christian <purin213@hotmail.com>
2024-05-15 08:15:19 +0900

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{env, process};
+
+use minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Could not parse argument: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
+
